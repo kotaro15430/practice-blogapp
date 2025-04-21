73577664c1702ab00af4f57340a0825d9879811b
DESKTOP-URPG87M\yuzan <stan.drt77@gamail.com>
2025-04-21 20:18:19 +0900

    データベースとか

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
     def index
-        @title = 'デイトラ'
-        # render 'home/index' Railsの慣習的に render は書かなくてOK
+        @article = Article.first
     end
 
     def about
